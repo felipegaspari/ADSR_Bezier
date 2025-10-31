c3c429cd5c7c4d8c0092c4437b2e1fd8dc881edb
Felipe Gaspari <felipe.gaspari@claro.com.ar>
2025-10-31 13:17:38 -0300

    initial commit for develop

---
+++ ADSR_Bezier.cpp
@@ -0,0 +1,11 @@
+//----------------------------------//
+// ADSR class for Arduino
+// by mo-thunderz
+// version 1.2
+// last update: 14.08.2020
+//----------------------------------//
+
+//#include "Arduino.h"
+//#include "adsr_fela_bezier.h"
+
+
